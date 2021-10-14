c88e942d96c9ef0916cc4ea864f94c09fca3112e
Alexis Michiels <alexis.michiels@student.vinci.be>
2021-10-14 15:15:20 +0200

    feat: implementation of "/series delete" commands

--- src/commands/subcommands/series-delete/blocks.ts
+++ src/commands/subcommands/series-delete/blocks.ts
@@ -1,5 +1,9 @@
 import {CommandInteraction} from 'discord.js';
 import {parseRawStringToNumberArray} from '../../../utils';
+import {getRawGuildBlockSeries} from '../../../utils';
+import {Guild} from 'discord.js';
+import {deleteGuildSeries} from '../../../utils';
+import {deleteSeries} from '../../../utils';
 
 export const execute = async (interaction: CommandInteraction) => {
 	const {
@@ -9,6 +13,17 @@ export const execute = async (interaction: CommandInteraction) => {
 	if (blocks === null) {
 		return await interaction.followUp('Every block has to be a number.');
 	}
+
+	const allSeries = await getRawGuildBlockSeries(interaction.guild as Guild, blocks);
+
+	const toDeleteSeries: string[] = [];
+
+	for (let series of allSeries) {
+		const id = await deleteGuildSeries(interaction.guild as Guild, series);
+		toDeleteSeries.push(id);
+	}
+
+	await deleteSeries(toDeleteSeries);
 
 	await interaction.followUp(`<@${interaction.user.id}> has deleted all series for blocks ${rawBlocks}.`);
 };
